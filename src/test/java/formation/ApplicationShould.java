0efe1f0b87167eb8c8446092d627ba573fe36fa0
Sepehr NAMDAR <sepehr_gsfc@yahoo.com>
2020-04-16 14:54:12 +0200

    init project

---
+++ src/test/java/formation/ApplicationShould.java
@@ -0,0 +1,12 @@
+package formation;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ApplicationShould {
+
+    @Test
+    public void test() {
+        Assertions.assertThat(true).isFalse();
+    }
+}
